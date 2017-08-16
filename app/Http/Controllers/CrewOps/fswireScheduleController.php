cbcf69f1458e42299ab1656ef28a734ee3f6e896
Mario Fehr <mario.fehr@gmx.at>
2017-08-16 21:17:31 +0200

    public schedule

---
+++ app/Http/Controllers/CrewOps/fswireScheduleController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\crewops;
+
+use App\AircraftGroup;
+use App\Airline;
+use App\Classes\VAOS_Schedule;
+use App\ScheduleTemplate;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class fswireScheduleController extends Controller
+{
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $airlines = Airline::all();
+        $acfgroups = AircraftGroup::all();
+        return view('admin.schedules.create', ['airlines' => $airlines, 'acfgroups' => $acfgroups]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // For now, just send the input to the controller.
+        // dd($request);
+        // Convert Request into Array
+
+        $this->validate($request, [
+            'flightnum' => 'required|numeric|digits:4',
+        ]);
+
+        $data = $request->all();
+        VAOS_Schedule::newRoute($data);
+
+        $request->session()->flash('schedule_created', true);
+        return redirect('flightops/schedule');
+    }
+}
