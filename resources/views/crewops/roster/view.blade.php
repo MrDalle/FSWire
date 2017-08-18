dd05e6e312e502cf4689f4e0af5fc61fda7f9220
Unknown <ddaletzki@hotmail.de>
2017-08-18 14:15:19 +0200

    Revert "Roster "
    This reverts commit 40e70c817ad7e586dd4e4ca2dc15b981340245c9.

--- resources/views/crewops/roster/view.blade.php
+++ resources/views/crewops/roster/view.blade.php
@@ -8,11 +8,6 @@
 <!-- Page Content -->
 <div class="content">
     <div class="row">
-<<<<<<< refs/remotes/origin/master
-=======
-
-      @foreach($users as $u)
->>>>>>> Roster
         <div class="col-sm-6 col-md-4 col-lg-3">
             <!-- Contact -->
             <div class="block block-rounded">
@@ -24,7 +19,6 @@
                             </button>
                         </li>
                     </ul>
-<<<<<<< refs/remotes/origin/master
                     <div class="block-title">Joshua Munoz</div>
                 </div>
                 <div class="block-content block-content-full bg-primary text-center">
@@ -79,13 +73,6 @@
                 <div class="block-content block-content-full bg-primary text-center">
                     <img class="img-avatar img-avatar-thumb" src="assets/img/avatars/avatar6.jpg" alt="">
                     <div class="font-s13 push-10-t">Photographer</div>
-=======
-                    <div class="block-title"><a href="{{ url('flightops/profile/' . $u->id) }}">{{ $u->username }}</a></div>
-                </div>
-                <div class="block-content block-content-full bg-primary text-center">
-                    <img class="img-avatar img-avatar-thumb" src="assets/img/avatars/avatar16.jpg" alt="">
-                    <div class="font-s13 push-10-t">{{ $u->id }}</div>
->>>>>>> Roster
                 </div>
                 <div class="block-content">
                     <div class="text-center push">
@@ -102,7 +89,6 @@
                     <table class="table table-borderless table-striped font-s13">
                         <tbody>
                             <tr>
-<<<<<<< refs/remotes/origin/master
                                 <td class="font-w600" style="width: 30%;">Category</td>
                                 <td>Work</td>
                             </tr>
@@ -114,25 +100,12 @@
                                 <td class="font-w600">Email</td>
                                 <td>user2@one.ui</td>
                             </tr>
-=======
-                                <td class="font-w600" style="width: 30%;">Total Flights</td>
-                                <td>{{ count($u->pirep) }}</td>
-                            </tr>
-                            <tr>
-                                <td class="font-w600">Miles Flown</td>
-                                <td>0</td>
-                            </tr>
->>>>>>> Roster
                         </tbody>
                     </table>
                 </div>
             </div>
             <!-- END Contact -->
         </div>
-<<<<<<< refs/remotes/origin/master
-=======
-@endforeach
->>>>>>> Roster
       </div>
   </div>
 
