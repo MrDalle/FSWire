5a629bd8b927b5420f9564005524506e68628d72
Unknown <ddaletzki@hotmail.de>
2017-08-18 14:34:25 +0200

    Revert "Roster update"
    This reverts commit 44f000b36e56d12dd5e07b0176668eb2558b32d2.

--- resources/views/crewops/roster/view.blade.php
+++ resources/views/crewops/roster/view.blade.php
@@ -8,10 +8,11 @@
 <!-- Page Content -->
 <div class="content">
     <div class="row">
-
+<<<<<<< refs/remotes/origin/master
+=======
 
       @foreach($users as $u)
-
+>>>>>>> Roster
         <div class="col-sm-6 col-md-4 col-lg-3">
             <!-- Contact -->
             <div class="block block-rounded">
@@ -23,8 +24,8 @@
                             </button>
                         </li>
                     </ul>
-
-                    <div class="block-title"><a href="{{ url('flightops/profile/' . $u->id) }}">{{ $u->username }}</a></div>
+<<<<<<< refs/remotes/origin/master
+                    <div class="block-title">Joshua Munoz</div>
                 </div>
                 <div class="block-content block-content-full bg-primary text-center">
                     <img class="img-avatar img-avatar-thumb" src="assets/img/avatars/avatar16.jpg" alt="">
@@ -45,16 +46,16 @@
                     <table class="table table-borderless table-striped font-s13">
                         <tbody>
                             <tr>
-                                <td class="font-w600" style="width: 30%;">Total Flights</td>
-                                <td>{{ count($u->pirep) }}</td>
+                                <td class="font-w600" style="width: 30%;">Category</td>
+                                <td>Work</td>
                             </tr>
                             <tr>
                                 <td class="font-w600">Phone</td>
                                 <td>+ 00 66008300</td>
                             </tr>
                             <tr>
-                                <td class="font-w600">AVG LDG </td>
-                                <td>{{ (INT) \App\PIREP::where('user_id', $u->id)->avg('landingrate') }}</td>
+                                <td class="font-w600">Email</td>
+                                <td>user1@one.ui</td>
                             </tr>
                         </tbody>
                     </table>
@@ -62,9 +63,76 @@
             </div>
             <!-- END Contact -->
         </div>
-
+        <div class="col-sm-6 col-md-4 col-lg-3">
+            <!-- Contact -->
+            <div class="block block-rounded">
+                <div class="block-header">
+                    <ul class="block-options">
+                        <li>
+                            <button type="button" data-toggle="modal" data-target="#modal-contact-edit">
+                                <i class="si si-pencil"></i>
+                            </button>
+                        </li>
+                    </ul>
+                    <div class="block-title">Ashley Welch</div>
+                </div>
+                <div class="block-content block-content-full bg-primary text-center">
+                    <img class="img-avatar img-avatar-thumb" src="assets/img/avatars/avatar6.jpg" alt="">
+                    <div class="font-s13 push-10-t">Photographer</div>
+=======
+                    <div class="block-title"><a href="{{ url('flightops/profile/' . $u->id) }}">{{ $u->username }}</a></div>
+                </div>
+                <div class="block-content block-content-full bg-primary text-center">
+                    <img class="img-avatar img-avatar-thumb" src="assets/img/avatars/avatar16.jpg" alt="">
+                    <div class="font-s13 push-10-t">{{ $u->id }}</div>
+>>>>>>> Roster
+                </div>
+                <div class="block-content">
+                    <div class="text-center push">
+                        <a class="text-default" href="javascript:void(0)">
+                            <i class="fa fa-2x fa-fw fa-facebook-square"></i>
+                        </a>
+                        <a class="text-info" href="javascript:void(0)">
+                            <i class="fa fa-2x fa-fw fa-twitter-square"></i>
+                        </a>
+                        <a class="text-danger" href="javascript:void(0)">
+                            <i class="fa fa-2x fa-fw fa-youtube-square"></i>
+                        </a>
+                    </div>
+                    <table class="table table-borderless table-striped font-s13">
+                        <tbody>
+                            <tr>
+<<<<<<< refs/remotes/origin/master
+                                <td class="font-w600" style="width: 30%;">Category</td>
+                                <td>Work</td>
+                            </tr>
+                            <tr>
+                                <td class="font-w600">Phone</td>
+                                <td>+ 00 43008422</td>
+                            </tr>
+                            <tr>
+                                <td class="font-w600">Email</td>
+                                <td>user2@one.ui</td>
+                            </tr>
+=======
+                                <td class="font-w600" style="width: 30%;">Total Flights</td>
+                                <td>{{ count($u->pirep) }}</td>
+                            </tr>
+                            <tr>
+                                <td class="font-w600">Miles Flown</td>
+                                <td>0</td>
+                            </tr>
+>>>>>>> Roster
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <!-- END Contact -->
+        </div>
+<<<<<<< refs/remotes/origin/master
+=======
 @endforeach
-
+>>>>>>> Roster
       </div>
   </div>
 
