e79943b8b369b18fc00a369cd7e4e1159a65e24b
Unknown <ddaletzki@hotmail.de>
2017-08-25 16:35:58 +0200

    donate update

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -27,11 +27,10 @@ We try to create smth new and indepent for bush flyers and Virtual Airline Pilot
                                 ">
                                 <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
                                 <img alt="" border="0" src="https://www.paypalobjects.com/de_DE/i/scr/pixel.gif" width="1" height="1">
-                                </form>
 
 
 
-                                <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+
 <input type="hidden" name="cmd" value="_s-xclick">
 <input type="hidden" name="hosted_button_id" value="4QKERX5KS8RAC">
 <input type="image" src="https://www.paypalobjects.com/de_DE/DE/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="Jetzt einfach, schnell und sicher online bezahlen – mit PayPal.">
