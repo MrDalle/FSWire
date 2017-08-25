f65bab3c8e97d88e2bac56d44882daf4ad0b0321
Unknown <ddaletzki@hotmail.de>
2017-08-25 20:09:57 +0200

    faq update 3

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -3,6 +3,7 @@
 
 
 <div class="content">
+  <div class="row">
             <!-- Frequently Asked Questions -->
             <div class="col-md-6">
                 <div class="block block-content block-content-full block-content-narrow">
@@ -27,9 +28,9 @@ We try to create smth new and indepent for bush flyers and Virtual Airline Pilot
                                 ">
                                 <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
                                 <img alt="" border="0" src="https://www.paypalobjects.com/de_DE/i/scr/pixel.gif" width="1" height="1">
+</form>
 
-
-
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 
 <input type="hidden" name="cmd" value="_s-xclick">
 <input type="hidden" name="hosted_button_id" value="4QKERX5KS8RAC">
@@ -463,7 +464,7 @@ We try to create smth new and indepent for bush flyers and Virtual Airline Pilot
 
 
 
-                            </div></div>
+                            </div></div></div>
 
 @endsection
 </body>
