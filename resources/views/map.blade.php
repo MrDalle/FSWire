706f25a66faeea6180563f2a016745021c3c4642
Mario Fehr <mario.fehr@gmx.at>
2017-09-12 10:32:03 +0200

    some css changes to the livemap

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -3,18 +3,20 @@
                 <!-- MAP -->
                 <style type="text/css">
 
-                .js-map-full iframe{
-                  height:1080px;
-                	width: 100%;
-
-                	position: relative; /* IE needs a position other than static */
-                }
+                    .js-map-full.custom {
+                        position: absolute;
+                        top: 60px;
+                        right: 0px;
+                        bottom: 0px;
+                        left: 0px;
+                        overflow: hidden;
+                    }
                 </style>
 
                 @extends('layouts.oneui')
                 @section('content')
                 <!-- MAP -->
-                <div class="js-map-full">
+                <div class="js-map-full custom">
                 <iframe scrolling="no" frameBorder="0" src="https://map.fswire.net"></iframe>
                 <!-- END MAP -->
                 </div>
