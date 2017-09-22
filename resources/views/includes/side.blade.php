a222d0d62faaedd59f6ab7cc3edd1dc78dd0a535
mrdalle <mrdalle@fswire.net>
2017-09-22 14:26:11 +0200

    New App Modal toggle Backend - Frontend

--- resources/views/includes/side.blade.php
+++ resources/views/includes/side.blade.php
@@ -531,45 +531,47 @@
     </ul>
     <!-- END Header Navigation Left -->
 
-
-<div class="modal fade in" id="apps-modal" tabindex="-1" role="dialog" aria-hidden="true" style="display: block;">
-    <div class="modal-sm modal-dialog modal-dialog-top">
-        <div class="modal-content">
-            <!-- Apps Block -->
-            <div class="block block-themed block-transparent">
-                <div class="block-header bg-primary-dark">
-                    <ul class="block-options">
-                        <li>
-                            <button data-dismiss="modal" type="button"><i class="si si-close"></i></button>
-                        </li>
-                    </ul>
-                    <h3 class="block-title">Apps</h3>
-                </div>
-                <div class="block-content">
-                    <div class="row text-center">
-                        <div class="col-xs-6">
-                            <a class="block block-rounded" href="{{ url('flightops') }}">
-                                <div class="block-content text-white bg-default">
-                                    <i class="si si-speedometer fa-2x"></i>
-                                    <div class="font-w600 push-15-t push-15">Backend</div>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="col-xs-6">
-                            <a class="block block-rounded" href="https://fswire.net">
-                                <div class="block-content text-white bg-modern">
-                                    <i class="si si-rocket fa-2x"></i>
-                                    <div class="font-w600 push-15-t push-15">Frontend</div>
-                                </div>
-                            </a>
+    <!-- Apps Modal -->
+    <!-- Opens from the button in the header -->
+    <div class="modal fade" id="apps-modal" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-sm modal-dialog modal-dialog-top">
+            <div class="modal-content">
+                <!-- Apps Block -->
+                <div class="block block-themed block-transparent">
+                    <div class="block-header bg-primary-dark">
+                        <ul class="block-options">
+                            <li>
+                                <button data-dismiss="modal" type="button"><i class="si si-close"></i></button>
+                            </li>
+                        </ul>
+                        <h3 class="block-title">Apps</h3>
+                    </div>
+                    <div class="block-content">
+                        <div class="row text-center">
+                            <div class="col-xs-6">
+                                <a class="block block-rounded" href="{{ url('flightops') }}">
+                                    <div class="block-content text-white bg-default">
+                                        <i class="si si-speedometer fa-2x"></i>
+                                        <div class="font-w600 push-15-t push-15">Backend</div>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="col-xs-6">
+                                <a class="block block-rounded" href="https://fswire.net">
+                                    <div class="block-content text-white bg-modern">
+                                        <i class="si si-rocket fa-2x"></i>
+                                        <div class="font-w600 push-15-t push-15">Frontend</div>
+                                    </div>
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
+                <!-- END Apps Block -->
             </div>
-            <!-- END Apps Block -->
         </div>
     </div>
-</div>
+    <!-- END Apps Modal -->
 
 </header>
 <!-- END Header -->
