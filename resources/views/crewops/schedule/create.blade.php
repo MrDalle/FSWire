1d69ecda390ff18cfe06070050d8549d658134c8
mrdalle <ddaletzki@hotmail.de>
2017-10-17 22:54:17 +0200

    responsive issues...

--- resources/views/crewops/schedule/create.blade.php
+++ resources/views/crewops/schedule/create.blade.php
@@ -200,7 +200,7 @@
                         </ul>
                         <h3 class=" h3 font-w700">Live WXR</h3>
                     </div>
-                <iframe width="606" height="550" src="https://embed.windy.com/embed2.html?lat=52.122&lon=8.960&zoom=5&level=surface&overlay=wind&menu=&message=&marker=&forecast=12&calendar=now&location=ip&type=map&actualGrid=&metricWind=kt&metricTemp=%C2%B0C" frameborder="0"></iframe>
+                <iframe width="100%" height="550" src="https://embed.windy.com/embed2.html?lat=52.122&lon=8.960&zoom=5&level=surface&overlay=wind&menu=&message=&marker=&forecast=12&calendar=now&location=ip&type=map&actualGrid=&metricWind=kt&metricTemp=%C2%B0C" frameborder="0"></iframe>
             </div>
             </div>
 
