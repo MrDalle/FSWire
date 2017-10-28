58f20b1b810fee5d3eddc200f183e9bee4b4cf48
mrdalle <ddaletzki@hotmail.de>
2017-10-28 11:04:18 +0200

    Uploaded clean Rank page for dev purpose

---
+++ resources/views/ranks.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.oneui')
+@section('content')
+
+
+                <!-- MAP -->
+                <!-- Maintenance Content -->
+                        <div class="content bg-white text-center pulldown overflow-hidden">
+                            <div class="row">
+                                <div class="col-sm-6 col-sm-offset-3">
+                                    <div class="font-s64 text-gray push-30-t push-50">
+                                        <i class="fa fa-cog fa-spin"></i>
+                                    </div>
+                                    <h1 class="h2 font-w400 push-15 animated fadeInLeftBig">Under Construction</h1>
+                                    <h2 class="h3 font-w300 text-dark-op push-50 animated fadeInRightBig">This feature will arrive soon!</h2>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- END Maintenance Content -->
+@endsection
+</body>
+
+
+</html>
