429124330bf862c3fb0807647e88cbd6460799ae
MrDalle <mrdale@fswire.net>
2017-11-05 19:45:44 +0100

    Typo fix shedules

--- resources/views/crewops/schedule/view.blade.php
+++ resources/views/crewops/schedule/view.blade.php
@@ -9,12 +9,11 @@
             <!-- Bootstrap Datepicker (.input-daterange class is initialized in App() -> uiHelperDatepicker()) -->
             <!-- For more info and examples you can check out https://github.com/eternicode/bootstrap-datepicker -->
             <div class="push-100-t push-100">
-                <h1 class="font-s48 font-w700 text-uppercase text-white push-10 text-center"
+                <h1 class="font-s48 font-w700 text-uppercase text-white push-10 text-center animated pulse"
                     data-toggle="appear" data-class="animated fadeInDown">Travel The World</h1>
-                <h2 class="h3 font-w400 text-white-op push-50 text-center" data-toggle="appear"
-                    data-timeout="750">Let us help you explore the world, one step at a time.</h2>
+
                 <div class="row">
-                    <div class="col-sm-8 col-sm-offset-2 col-lg-6 col-lg-offset-3">
+                    <div class="col-sm-8 col-sm-offset-2 col-lg-6 col-lg-offset-3 animated pulse">
                         <div class="block">
                             <ul class="nav nav-tabs nav-justified" data-toggle="tabs">
                                 <li class="active">
